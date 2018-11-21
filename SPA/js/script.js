642ca9cf6462a3f52a605dfdcd1957ffee2e2dcf
Vanesa Paniego <vanesa.paniego@trappit.com>
2018-11-21 20:51:12 +0100

    Ajax Login + ApiRest

--- SPA/js/script.js
+++ SPA/js/script.js
@@ -1,17 +1,46 @@
-$(document).ready(function(){
-    $('#idLogin').click( function(){        
-        $('.content').load('login.html');   
+$(document).ready(function () {
+
+    $('#idLogin').click(function () {
+        $('.content').load('login.html');
     });
-    $('#idRegistro').click( function(){     
-        $('.content').load('registro.html');  
+
+    $('#idRegistro').click(function () {
+        $('.content').load('registro.html');
     });
-    $('#idReservar').click( function(){     
-        $('.content').load('reservar.html');  
+
+    $('#idReservar').click(function () {
+        $('.content').load('reservar.html');
     });
-    $('#idInstalaciones').click( function(){     
-        $('.content').load('instalaciones.html');  
+
+    $('#idInstalaciones').click(function () {
+        $('.content').load('instalaciones.html');
     });
-    $('#idServicios').click( function(){     
-        $('.content').load('servicios.html');  
-    });    
+
+    $('#idServicios').click(function () {
+        $('.content').load('servicios.html');
+    });
+
+    $('#loginForm').submit(function (event) {
+        event.preventDefault();
+        var url = "http://fenw.etsisi.upm.es:5555/users/login";
+        var username = $('#inputUsername').val();
+        var password = $('#inputPassword').val();
+        url = url + "?username=" + username + "&password=" + password;
+        console.log("Prueba url correcta : " + url);
+
+        $.ajax({
+            url: url,
+            type: "GET",
+            async: true,
+            dataType: "json"
+        })
+            .done(function (data) {
+                console.log("Usuario logado");
+            })
+            .fail(function (data) {
+                console.log("Usuario no logado");
+            });
+    });
+
+
 });
