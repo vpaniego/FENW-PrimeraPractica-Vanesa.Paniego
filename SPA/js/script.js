8a4ec130860a1046b457ec072f7393360764f6ed
Vanesa Paniego <vanesa.paniego@trappit.com>
2018-11-24 18:41:51 +0100

    Refactoring script.js

--- SPA/js/script.js
+++ SPA/js/script.js
@@ -1,71 +1,15 @@
+var urlApi = "http://fenw.etsisi.upm.es:5555";
+var ajaxAsync = true;
+var ajaxDataType = "json";
+
 $(function () {
-
     hideComponentsNavBar();
-
-    $('#idLogin').click(function () {
-        $('.content').load('login.html', function (responseTxt, statusTxt, xhr) {
-            if (statusTxt == "success") {
-
-                hideComponentsAlert();
-
-                $('#loginForm').submit(function (event) {
-                    event.preventDefault();
-                    var url = "http://fenw.etsisi.upm.es:5555/users/login";
-                    var username = $('#inputUsername').val();
-                    var password = $('#inputPassword').val();
-                    url = url + "?username=" + username + "&password=" + password;
-
-                    $.ajax({
-                        url: url,
-                        type: "GET",
-                        async: true,
-                        dataType: "json"
-                    })
-                        .done(function (response, textStatus, xhr) {
-                            console.log("Usuario logado correctamente");
-
-                            var auth = xhr.getResponseHeader('Authorization');
-
-                            sessionStorage.setItem("datalogin", auth);
-
-                            alertSuccessLogin();
-                        })
-                        .fail(function (error) {
-                            console.log("Usuario no logado");
-
-                            alertErrorLogin();
-                        });
-                });
-            }
-            if (statusTxt == "error") {
-                alert("Error: " + xhr.status + ": " + xhr.statusText);
-            }
-
-
-        });
-    });
-
-    $('#idRegistro').click(function () {
-        $('.content').load('registro.html');
-    });
-
-    $('#idReservar').click(function () {
-        let usuarioLogado = sessionStorage.getItem("datalogin");
-        if (usuarioLogado) {
-            $('.content').load('reservar.html');
-        }
-    });
-
-    $('#idInstalaciones').click(function () {
-        $('.content').load('instalaciones.html');
-    });
-
-    $('#idServicios').click(function () {
-        $('.content').load('servicios.html');
-    });
-
+    $('#idLogin').click(loadLogin);
+    $('#idRegistro').click(loadRegistro);
+    $('#idReservar').click(loadReservar);
+    $('#idInstalaciones').click(loadInstalaciones);
+    $('#idServicios').click(loadServicios);
     $('#idLogout').click(logout);
-
 });
 
 function hideComponentsNavBar() {
@@ -95,10 +39,110 @@ function alertErrorLogin() {
     $('#successLogin').hide();
 }
 
-function logout() {
-    sessionStorage.removeItem("datalogin");
+function loadInicio() {
+    $(location).attr('href', 'index.html');
+}
 
+function loadServicios() {
+    loadComponent('servicios.html');
+}
+
+function loadInstalaciones() {
+    loadComponent('instalaciones.html');
+}
+
+function loadReservar() {
+    let myToken = getToken("datalogin");
+    if (myToken) {
+        loadComponent('reservar.html');
+    }
+}
+
+function loadRegistro() {
+    $('.content').load('registro.html', function (responseTxt, statusTxt, xhr) {
+        if (statusTxt == "success") {
+            $('#registryForm').submit(function (event) {
+                // Comprobacion para evitar Spam en el envio del formulario de registro
+                var controlSpam = $('#nospam').val();
+                if (controlSpam) {
+                    console.log("El campo de control de spam está vacío. Se realiza el envío del formulario de registro de usuario");
+
+                    //TODO: POST /users
+
+                } else {
+                    console.log("El campo de control de spam no está vacío. Es un spam bot!!!");
+                }
+            });
+        }
+        if (statusTxt == "error") {
+            alert("Error: " + xhr.status + ": " + xhr.statusText);
+        }
+    });
+}
+
+function loadLogin() {
+    $('.content').load('login.html', function (responseTxt, statusTxt, xhr) {
+        if (statusTxt == "success") {
+            hideComponentsAlert();
+            $('#loginForm').submit(function (event) {
+                event.preventDefault();
+                var operacion = "/users/login";
+                var username = $('#inputUsername').val();
+                var password = $('#inputPassword').val();
+                urlApi = urlApi + operacion + "?username=" + username + "&password=" + password;
+                $.ajax({
+                    url: urlApi,
+                    type: "GET",
+                    async: ajaxAsync,
+                    dataType: ajaxDataType
+                })
+                    .done(function (response, textStatus, xhr) {
+                        console.log("Usuario logado correctamente");
+                        tratarToken(response, xhr);
+                        alertSuccessLogin();
+                    })
+                    .fail(function (error) {
+                        console.log("Usuario no logado");
+                        alertErrorLogin();
+                    });
+            });
+        }
+        if (statusTxt == "error") {
+            console.log("Error: " + xhr.status + ": " + xhr.statusText);
+        }
+    });
+}
+
+function tratarToken(response, xhr) {
+    let auth = xhr.getResponseHeader('Authorization');
+    if (auth === response) {
+        saveToken("datalogin", auth);
+        console.log('Token verificado');
+    } else {
+        console.log('Token no verificado')
+    }
+}
+
+function loadComponent(url) {
+    $('.content').load(url);
+}
+
+function logout() {
+    deleteToken("datalogin");
     toggleComponentsNavBar();
-    hideComponentsAlert()
+    hideComponentsAlert();
+    loadInicio();
+}
+
+function saveToken(key, value) {
+    sessionStorage.setItem(key, value);
+}
+
+function deleteToken(key) {
+    sessionStorage.removeItem(key);
+}
+
+function getToken(key) {
+    return sessionStorage.getItem(key);
 }
 
